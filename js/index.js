1637c1865e8b9bed0173054813c6a58a96a92958
Joseph Ortiz <jortiz@lat-jortiz.gateway.2wire.net>
2014-09-26 13:21:01 -0400

    added question list

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
 (function () {
-   'use strict';
-   // this function is strict...  
+  'use strict';
 }());
+var questions = [{
+  text: "Which country has won the most Olympic Medals overall?",
+  choices: ["United States", "Russia", "Germany", "Great Britain"],
+  answer: "United States"
+}, {
+  text: "The kendama is a traditional ball and cup toy originating from what asian country?",
+  choices: ["China", "Japan", "Korea", "Taiwan"],
+  answer: "Japan"
+}, {
+  text: "The smallest country in the world is ...",
+  choices: ["Monaco", "Nauru", "San Marino", "Vatican City"],
+  answer: "Vatican City"
+}, {
+  text: "What country is the most densley populated?",
+  choices: ["Singapore", "Monaco", "Bangladesh", "Malta"],
+  answer: "Bangladesh"
+}, {
+  text: "What country has the most cars?",
+  choices: ["Japan", "Germany", "United States", "Australia"],
+  answer: "United States"
+}];
